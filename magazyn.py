3d9b8b36a81712cea4e3afa2c23bdd0704cdcf7e
Michał Jędrzejak <mjedrzejak77@gmail.com>
2022-03-11 19:09:34 +0100

    start with welcome

---
+++ magazyn.py
@@ -0,0 +1,8 @@
+#def store_items(items):
+    #items = ("Ibufen", "Devicap", "Pirolam")
+
+welcome = input("What would You like to do?")
+if welcome == "exit":
+    print("Exiting...Bye")
+else:
+    input("What would You like to do?")
