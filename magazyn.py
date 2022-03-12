1d8afc70d17a4b516b83f3d64e9a6a4b549d7dfe
Michał Jędrzejak <mjedrzejak77@gmail.com>
2022-03-12 19:32:04 +0100

    added products list

--- magazyn.py
+++ magazyn.py
@@ -1,8 +1,23 @@
-#def store_items(items):
-    #items = ("Ibufen", "Devicap", "Pirolam")
+import sys
 
-welcome = input("What would You like to do?")
-if welcome == "exit":
-    print("Exiting...Bye")
-else:
-    input("What would You like to do?")
+products_list = []
+
+products_list.append({"name":"Ibufen", "quantity":20, "unit":"kg", "unit_price":13})
+products_list.append({"name":"Devicap", "quantity":13, "unit":"kg", "unit_price":42.5})
+products_list.append({"name":"Pirolam", "quantity":15, "unit":"op", "unit_price":17})
+
+
+welcome = ''
+while welcome != 'exit':
+    welcome = input("What would You like to do? ")
+print("Exiting...Bye")
+exit(0)
+
+def get_items():
+    if welcome == 'show':
+        print(products_list)
+
+showing = get_items(products_list)
+print(showing)
+
+#print("Name\tQuantity\tUnit\tUnit Price (PLN)")
